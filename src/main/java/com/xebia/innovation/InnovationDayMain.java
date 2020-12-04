35a73d3ce864ab53d175d046cb5fc16c3b8fcbd7
Ruben Oostinga <roostinga@xebia.com>
2020-12-04 21:48:45 +0100

    Working retry policy

--- src/main/java/com/xebia/innovation/InnovationDayMain.java
+++ src/main/java/com/xebia/innovation/InnovationDayMain.java
@@ -1,6 +1,8 @@
 package com.xebia.innovation;
 
 import com.uber.cadence.activity.ActivityMethod;
+import com.uber.cadence.activity.ActivityOptions;
+import com.uber.cadence.client.WorkflowClient;
 import com.uber.cadence.common.RetryOptions;
 import com.uber.cadence.worker.Worker;
 import com.uber.cadence.worker.WorkerOptions;
@@ -21,31 +23,11 @@ import java.util.List;
 import static com.uber.cadence.samples.common.SampleConstants.DOMAIN;
 
 public class InnovationDayMain {
-
-    public interface InnovationDayWorkflow {
-        @WorkflowMethod(executionStartToCloseTimeoutSeconds = 300)
-        void callUnreliableService() throws IOException, InterruptedException;
-    }
+    static final String TASK_LIST = "innovationDayTaskList";
 
     public interface RestActivities {
-        @ActivityMethod()
+        @ActivityMethod(scheduleToCloseTimeoutSeconds = 300)
         String callRestService();
-    }
-
-    public static class InnovationDayWorkflowImpl implements InnovationDayWorkflow {
-        private final RestActivities activities =
-                Workflow.newActivityStub(RestActivities.class);
-        @Override
-        public void callUnreliableService() {
-            List<Promise<String>> promiseList = new ArrayList<>();
-            for(int i = 0; i < 100; i++) {
-                promiseList.add(Workflow.retry(
-                        new RetryOptions.Builder().setInitialInterval(Duration.ofSeconds(1)).setMaximumAttempts(10).build(),
-                        () -> Async.function(activities::callRestService)
-                ));
-            }
-            Promise.allOf(promiseList).get();
-        }
     }
 
     static class RestActivitiesImpl implements RestActivities {
@@ -54,15 +36,15 @@ public class InnovationDayMain {
             try {
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://httpbin.org/status/500,200"))
-                        .build();
+                    .uri(URI.create("https://httpbin.org/status/500,200"))
+                    .build();
                 HttpResponse<String> send = client.send(
-                        request,
-                        HttpResponse.BodyHandlers.ofString()
+                    request,
+                    HttpResponse.BodyHandlers.ofString()
                 );
 
                 if (send.statusCode() == 500) {
-                    throw new Error("service call failed");
+                    throw new RuntimeException("service call failed");
                 }
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -72,15 +54,64 @@ public class InnovationDayMain {
         }
     }
 
-    public static void main(String[] args) {
+    public interface InnovationDayWorkflow {
+        @WorkflowMethod(executionStartToCloseTimeoutSeconds = 300, taskList = TASK_LIST)
+        void callUnreliableService() throws IOException, InterruptedException;
+    }
+
+
+    public static class InnovationDayWorkflowImpl implements InnovationDayWorkflow {
+        private final RestActivities activities =
+            Workflow.newActivityStub(
+                RestActivities.class,
+                new ActivityOptions.Builder()
+                    .setRetryOptions(
+                        new RetryOptions.Builder()
+                            .setInitialInterval(Duration.ofSeconds(1))
+                            .setBackoffCoefficient(1.2)
+                            .setMaximumAttempts(10)
+                            .build()
+                    )
+                    .build());
+
+
+        @Override
+        public void callUnreliableService() {
+            List<Promise<String>> promiseList = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                promiseList.add(
+                    Async.function(activities::callRestService)
+                );
+            }
+            Promise.allOf(promiseList).get();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         Worker.Factory workerFactory = new Worker.Factory(DOMAIN);
-        Worker worker = workerFactory.newWorker("innovationDayTaskList", new WorkerOptions.Builder()
-                .setTaskListActivitiesPerSecond(1)
-                .build());
+        Worker worker = workerFactory.newWorker(TASK_LIST, new WorkerOptions.Builder()
+            .setTaskListActivitiesPerSecond(1)
+            .build());
         worker.registerWorkflowImplementationTypes(
             InnovationDayWorkflowImpl.class
         );
         worker.registerActivitiesImplementations(new RestActivitiesImpl());
         workerFactory.start();
+
+        RetryOptions retryOptions = new RetryOptions.Builder()
+            .setInitialInterval(Duration.ofSeconds(1))
+            .setMaximumAttempts(10)
+            .build();
+        // Start a workflow execution. Usually this is done from another program.
+        WorkflowClient workflowClient = WorkflowClient.newInstance(DOMAIN);
+        // Get a workflow stub using the same task list the worker uses.
+        InnovationDayWorkflow innovationDayWorkflow = workflowClient.newWorkflowStub(
+            InnovationDayWorkflow.class
+//            new WorkflowOptions.Builder().setRetryOptions(retryOptions).build()
+        );
+        // Execute a workflow waiting for it to complete.
+        innovationDayWorkflow.callUnreliableService();
+        System.exit(0);
     }
 }
