675f6cbea89c4096d9b4232966b686e874924d54
Sefa Şentürk <senturk.sefa@gmail.com>
2020-12-04 13:52:50 +0100

    Add retry on workflow

--- src/main/java/com/xebia/innovation/InnovationDayWorkflowImpl.java
+++ src/main/java/com/xebia/innovation/InnovationDayWorkflowImpl.java
@@ -1,25 +1,36 @@
 package com.xebia.innovation;
 
+import com.uber.cadence.common.RetryOptions;
+import com.uber.cadence.workflow.Workflow;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 
 public class InnovationDayWorkflowImpl implements InnovationDayWorkflow {
     @Override
-    public void callUnreliableService() throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://httpbin.org/status/500,200"))
-            .build();
-        HttpResponse<String> send = client.send(
-            request,
-            HttpResponse.BodyHandlers.ofString()
-        );
-        if (send.statusCode() <= 500 && send.statusCode() < 600) {
-            throw new Error("service call failed");
-        }
+    public void callUnreliableService() {
+        Workflow.retry(new RetryOptions.Builder().setInitialInterval(Duration.ofSeconds(1)).setMaximumAttempts(10).build(), () -> {
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("https://httpbin.org/status/500,200"))
+                        .build();
+                HttpResponse<String> send = client.send(
+                        request,
+                        HttpResponse.BodyHandlers.ofString()
+                );
+
+                if (send.statusCode() == 500) {
+                    throw new Error("service call failed");
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
